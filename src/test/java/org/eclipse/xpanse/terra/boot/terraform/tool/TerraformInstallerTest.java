5d35f3ea63be61508b794247452be5053d232cb4
Alice1319 <alice_lai1319@outlook.com>
2025-03-26 18:42:14 +0800

    Add similar test but with GitHub API mocked with wiremock. (#217)

--- src/test/java/org/eclipse/xpanse/terra/boot/terraform/tool/TerraformInstallerTest.java
+++ src/test/java/org/eclipse/xpanse/terra/boot/terraform/tool/TerraformInstallerTest.java
@@ -1,14 +1,22 @@
 package org.eclipse.xpanse.terra.boot.terraform.tool;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import jakarta.annotation.Resource;
 import java.io.File;
 import java.util.Set;
 import org.eclipse.xpanse.terra.boot.models.exceptions.InvalidTerraformToolException;
 import org.eclipse.xpanse.terra.boot.terraform.utils.SystemCmd;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,8 +29,13 @@ import org.springframework.boot.test.context.SpringBootTest;
             TerraformVersionsFetcher.class,
             SystemCmd.class
         },
-        properties = {"support.default.terraform.versions.only=true"})
+        properties = {
+            "support.default.terraform.versions.only=true",
+            "terraform.versions.github.api.url=http://localhost:8089"
+        })
 class TerraformInstallerTest {
+
+    private static WireMockServer wireMockServer;
 
     @Value("${terraform.default.supported.versions:1.6.0,1.7.0,1.8.0,1.9.0,1.10.0}")
     private String terraformVersions;
@@ -31,8 +44,80 @@ class TerraformInstallerTest {
     @Resource private TerraformVersionsHelper versionHelper;
     @Resource private TerraformVersionsCache versionsCache;
 
+    @Disabled
     @Test
     void testGetExecutableTerraformByVersion() {
+        Set<String> defaultVersions = Set.of(terraformVersions.split(","));
+        Set<String> cachedVersions = versionsCache.getAvailableVersions();
+        assertTrue(cachedVersions.containsAll(defaultVersions));
+        assertTrue(cachedVersions.size() >= defaultVersions.size());
+
+        String requiredVersion = "";
+        String terraformPath = installer.getExecutorPathThatMatchesRequiredVersion(requiredVersion);
+        assertEquals("terraform", terraformPath);
+
+        String requiredVersion1 = "= 1.7.0";
+        String[] operatorAndNumber1 =
+                versionHelper.getOperatorAndNumberFromRequiredVersion(requiredVersion1);
+        String terraformPath1 =
+                installer.getExecutorPathThatMatchesRequiredVersion(requiredVersion1);
+        assertTrue(
+                versionHelper.checkIfExecutorIsMatchedRequiredVersion(
+                        new File(terraformPath1), operatorAndNumber1[0], operatorAndNumber1[1]));
+
+        String requiredVersion2 = "<= v1.6.0";
+        String[] operatorAndNumber2 =
+                versionHelper.getOperatorAndNumberFromRequiredVersion(requiredVersion2);
+        String terraformPath2 =
+                installer.getExecutorPathThatMatchesRequiredVersion(requiredVersion2);
+        assertTrue(
+                versionHelper.checkIfExecutorIsMatchedRequiredVersion(
+                        new File(terraformPath2), operatorAndNumber2[0], operatorAndNumber2[1]));
+
+        String requiredVersion3 = ">= v1.9.0";
+        String[] operatorAndNumber3 =
+                versionHelper.getOperatorAndNumberFromRequiredVersion(requiredVersion3);
+        String terraformPath3 =
+                installer.getExecutorPathThatMatchesRequiredVersion(requiredVersion3);
+        assertTrue(
+                versionHelper.checkIfExecutorIsMatchedRequiredVersion(
+                        new File(terraformPath3), operatorAndNumber3[0], operatorAndNumber3[1]));
+
+        String requiredVersion4 = ">= 100.0.0";
+        assertThrows(
+                InvalidTerraformToolException.class,
+                () -> installer.getExecutorPathThatMatchesRequiredVersion(requiredVersion4));
+    }
+
+    @BeforeAll
+    static void setupWireMockServer() {
+        wireMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().port(8089));
+        wireMockServer.start();
+
+        wireMockServer.stubFor(
+                get(urlPathEqualTo("/repos/hashicorp/terraform/tags"))
+                        .willReturn(
+                                aResponse()
+                                        .withStatus(200)
+                                        .withHeader("Content-Type", "application/json")
+                                        .withBody(
+                                                """
+                                                [
+                                                    {"name": "v1.7.0"},
+                                                    {"name": "v1.8.0"},
+                                                    {"name": "v1.9.0"},
+                                                    {"name": "v1.10.0"}
+                                                ]
+                                                """)));
+    }
+
+    @AfterAll
+    static void teardownWireMockServer() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    void testGetExecutableTerraformByVersionWithWiremock() {
         Set<String> defaultVersions = Set.of(terraformVersions.split(","));
         Set<String> cachedVersions = versionsCache.getAvailableVersions();
         assertTrue(cachedVersions.containsAll(defaultVersions));
